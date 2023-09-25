c69d45f43dc67784550af45e6664efa71b438390
Kelvin963 <sousaks054@gmail.com>
2023-09-25 13:19:40 -0300

    At.Avaliativa4

---
+++ Atividade4/Atividade4.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main (){
+
+char nivel;
+double salario;
+double a = 0.05;
+double b = 0.07;
+double c = 0.08;
+
+printf ("Digite o salário do funcionario:\n");
+scanf ("%lf", &salario);
+
+printf("Digite o nivel do funcionario:\n");
+scanf(" %c", &nivel);  // precisa do espaço antes do %
+
+if (nivel == 'a'){
+    printf("Salario com aumento de 5 por cento:R$ %.2lf", salario * a + salario);
+
+}else if(nivel == 'b'){
+    printf("Salario com aumento de 7 por cento:R$ %.2lf", salario * b + salario);
+
+}else if(nivel == 'c'){
+    printf("Salario com aumento de 8 por cento:R$ %.2lf", salario * c + salario);
+
+}else{
+
+    printf("invalido");
+}
+
+
+
+
+
+
+
+/*a = a * salario;
+a = a + salario;
+
+b = b * salario;
+b = b + salario;
+                        --testes 
+c = c * salario;
+c = c + salario;
+
+
+printf("Salario com aumento de 5 por cento:R$%.2lf\n", a);
+printf("Salario com aumento de 7 por cento:R$%.2lf\n", b);
+printf("Salario com aumento de 8 por cento:R$%.2lf\n", c);
+*/
+
+
+
+
+
+
+return 0;
+}
